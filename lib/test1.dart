196312cf01ebecf8095390e58bf5bd1c301694ce
unknown <Gialinh0167@gmail.com>
2025-03-26 13:42:17 +0700

    the last update

--- lib/test1.dart
+++ lib/test1.dart
@@ -9,14 +9,21 @@ class NFAe {
 
   Set<int> getClosure(Set<int> states) {
     Set<int> closure = Set.from(states);
-    List<int> nextState = List.from(states);
-    while(nextState.isNotEmpty){
-      
-      int state = nextState.removeAt(0);
-      if(transition.containsKey(state)){
-        if(transition[state]!.containsKey('s')){
-          nextState.addAll(transition[state]!['s']!);
-          closure.addAll(transition[state]!['s']!);
+    Set<int> nextState = Set.from(states);
+    while (nextState.isNotEmpty) {
+      int state = nextState.first;
+      nextState.remove((state));
+      if (transition.containsKey(state)) {
+        if (transition[state]!.containsKey('s')) {
+          for(int i in transition[state]!['s']!){
+            if(!closure.contains(i)){
+              closure.add(i);
+              nextState.add(i);
+            }
+            // print("state: $i");
+            // print("next state $nextState");
+          }
+          
         }
       }
     }
@@ -117,51 +124,51 @@ class NFAe {
 }
 
 void main() {
-  Set<int> states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-  Set<String> alphabet = {'a', 'b'};
-  Map<int, Map<String, Set<int>>> transition = {
-    0: {
-      's': {1,7},
-    },
-    1: {
-      's': {2,4},
-    },
-    2: {
-      // 's': {2},
-      'a': {3}
-    },
-    3: {
-      's': {6},
-    },
-    4: {
-      // 's': {4},
-      'b': {5}
-    },
-    5: {
-      's': {6},
-    },
-    6: {
-      's': {1,7},
-    },
-    7: {
-      // 's': {7},
-      'a': {8}
-    },
-    8: {
-      // 's': {8},
-      'b': {9}
-    },
-    9: {
-      // 's': {9},
-      'b': {10}
-    },
-    10: {
-      // 's': {10},
-    },
-  };
-  NFAe nfae = NFAe(states, alphabet, transition, 0, {10});
-  // print(nfae.getClosure({0}));
-  print(nfae.buildDFA());
+  // Set<int> states = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  // Set<String> alphabet = {'a', 'b'};
+  // Map<int, Map<String, Set<int>>> transition = {
+  //   0: {
+  //     's': {1,7},
+  //   },
+  //   1: {
+  //     's': {2,4},
+  //   },
+  //   2: {
+  //     // 's': {2},
+  //     'a': {3}
+  //   },
+  //   3: {
+  //     's': {6},
+  //   },
+  //   4: {
+  //     // 's': {4},
+  //     'b': {5}
+  //   },
+  //   5: {
+  //     's': {6},
+  //   },
+  //   6: {
+  //     's': {1,7},
+  //   },
+  //   7: {
+  //     // 's': {7},
+  //     'a': {8}
+  //   },
+  //   8: {
+  //     // 's': {8},
+  //     'b': {9}
+  //   },
+  //   9: {
+  //     // 's': {9},
+  //     'b': {10}
+  //   },
+  //   10: {
+  //     // 's': {10},
+  //   },
+  // };
+  // NFAe nfae = NFAe(states, alphabet, transition, 0, {10});
+  // // print(nfae.getClosure({0}));
+  // print(nfae.buildDFA());
 
   // Set<int> states = {0, 1, 2};
   // Set<String> alphabet = {'a', 'b'};
@@ -186,22 +193,22 @@ void main() {
 
   // print(nfae.buildDFA());
 
-  // Set<int> states = {0, 1, 2};
-  // Set<String> alphabet = {'a', 'b', 'c'};
-  // Map<int, Map<String, Set<int>>> transition = {
-  //   0: {
-  //     's': {1},
-  //     'a': {0},
-  //   },
-  //   1: {
-  //     's': {2},
-  //     'b': {1},
-  //   },
-  //   2: {
-  //     'c': {2}
-  //   },
-  // };
-  // NFAe nfae = NFAe(states, alphabet, transition, 0, {1});
-  // print(nfae.getClosureTest({1}));
+  Set<int> states = {0, 1, 2};
+  Set<String> alphabet = {'a', 'b', 'c'};
+  Map<int, Map<String, Set<int>>> transition = {
+    0: {
+      's': {1,2},
+      'a': {0},
+    },
+    1: {
+      's': {0},
+      'b': {1},
+    },
+    2: {
+      'c': {2}
+    },
+  };
+  NFAe nfae = NFAe(states, alphabet, transition, 0, {1});
+  print(nfae.getClosure({0}));
   // print(nfae.buildDFA());
 }
